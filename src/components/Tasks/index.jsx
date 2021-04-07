3d2e5dd115b0c875d96c01aa21ef53bfeeb5c08e
BangtanAi <kairova-94@mail.ru>
2021-04-08 00:19:46 +0600

    Разработка списка задача на ReactJS (ToDo) #6. Скопировала готовый код с гитхаба для таск и css для него

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -2,6 +2,7 @@ import './Tasks.scss';
 import editSvg from '../../assets/img/edit.svg';
 import axios from 'axios';
 import AddTaskForm from './AddTaskForm';
+import Task from './Task';
 
 const Tasks = ( { list, onEditTitle, onAddTask, withoutEmpty } ) => {
     const editTitle = () => {
@@ -27,16 +28,7 @@ const Tasks = ( { list, onEditTitle, onAddTask, withoutEmpty } ) => {
         <div className='tasks__items'>
             {!withoutEmpty && !list.tasks.length && <h2>Задачи отсутствуют</h2> }
             {list.tasks.map(task => (
-                <div key={task.id} className='tasks__items-row'>
-                <div className='checkbox'>
-                    <input id={`task-${task.id}`} type='checkbox' />
-                    <label htmlFor={`task-${task.id}`}>
-                    <svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" stroke="#000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                    </svg></label>              
-                </div>
-                <input readOnly value={task.text} />
-                </div>
+                <Task key={task.id} task = {task} />
             ))}
             <AddTaskForm list={list} onAddTask={onAddTask} />
             
